996ef720b9070828e51909481ccb944553b7d7d3
Kiril Pecev <kir4o_pecev@abv.bg>
2018-03-09 22:26:04 +0200

    added problem one solution

---
+++ into-and-basic-syntax/exercises/Problem 1. Debit Card Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a:d4} {b:d4} {c:d4} {d:d4}");
+        }
+    }
+}
