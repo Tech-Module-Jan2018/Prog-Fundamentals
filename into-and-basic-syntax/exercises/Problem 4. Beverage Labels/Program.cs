3b7926091ec5862b3a9d437c1bbcd27b406f89bd
Kiril Pecev <kir4o_pecev@abv.bg>
2018-03-09 22:29:37 +0200

    added and other problems

---
+++ into-and-basic-syntax/exercises/Problem 4. Beverage Labels/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{volume/100.0*energyContent}kcal, {volume / 100.0 * sugarContent}g sugars");
+        }
+    }
+}
